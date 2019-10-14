8899a1511e6627cae83561a144252d63ab2394fc
Sean <sjgallen@gmail.com>
2019-10-13 21:43:29 -0500

    moved components into one folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './component/app.component';
 import { InputPageComponent } from './component/input-page/input-page.component';
 
 @NgModule({
