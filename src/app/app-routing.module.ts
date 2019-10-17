b633131813e62969429fbce225404e229ae8df30
Sean <sjgallen@gmail.com>
2019-10-16 20:45:14 -0500

    added better css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InputPageComponent } from './component/input-page/input-page.component';
+import {MatInputModule} from '@angular/material/input';
 
 const routes: Routes = [
   {
@@ -10,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), MatInputModule],
+  exports: [RouterModule, MatInputModule]
 })
 export class AppRoutingModule { }
