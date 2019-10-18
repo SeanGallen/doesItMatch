07f9f8687e78cb4e7adbb11b9bc163af49fcaac5
Sean <sjgallen@gmail.com>
2019-10-17 19:01:30 -0500

    updated the button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InputPageComponent } from './component/input-page/input-page.component';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 const routes: Routes = [
   {
@@ -11,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), MatInputModule],
-  exports: [RouterModule, MatInputModule]
+  imports: [RouterModule.forRoot(routes), MatInputModule, MatButtonModule],
+  exports: [RouterModule, MatInputModule, MatButtonModule]
 })
 export class AppRoutingModule { }
