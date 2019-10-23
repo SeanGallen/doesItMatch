8dbe3fbc0d4fc5efb1f4a67534df327345ac1f49
Sean <sjgallen@gmail.com>
2019-10-22 22:48:30 -0500

    trying to add color or line numbers

--- src/app/text-match-service.ts
+++ src/app/text-match-service.ts
@@ -18,5 +18,25 @@ export class TextMatchService {
       this.equalityResponse['texts'] = this.texts;
       return this.equalityResponse;
     }
+    else{
+      var count0 = 1;
+      var count1 = 1;
+      for (let index = 0; index < this.texts[0].length; index++) {
+        let element1 = this.texts[0][index];
+        let element2 = this.texts[1][index];
+        if(element1 === '\n') {
+          element1 = element1 + ' ' + count0;
+          count0 += 1;
+        }
+        if(element2 === '\n') {
+          element2 = (element2 + ' ' + count1);
+          count1+= 1;
+        }
+        console.log(element2);
+      }
+      this.equalityResponse['valid'] = false;
+      this.equalityResponse['texts'] = this.texts; 
+      return this.equalityResponse;    
+    }
   }
 }
